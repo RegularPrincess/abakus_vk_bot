9b23d8d617f8baf374de5a7b95998db5853d8a33
JMFedorov <jmfedorov@gmail.com>
2018-08-08 15:30:48 +0300

    new geo bot

---
+++ config.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+token = '76c156aee6d663381831389817defc594ec923edd09ae8fb2cc160624858d091fbb0f6628ba12177bef54'
+
+confirmation_token = '7e0b4572' # Group confirmation token
+secret_key = 'vErYsecreT228' # Group secret key
+group_id = 169862413  # VK group id
+admin_id = 259056624
+admin_name = 'Юрий Федоров'
+vk_api_url = 'https://api.vk.com/method/'
+db_name = 'db.db'
+bot_name = 'geo_enroll_vk_bot'
+api_ver = '5.80'
