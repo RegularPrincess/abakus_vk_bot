7bbb02b206cd02cd68fa823045200781aa9df6cd
JMFedorov <jmfedorov@gmail.com>
2018-09-11 18:02:55 +0300

    remove port nedded

--- config.py
+++ config.py
@@ -12,3 +12,7 @@ vk_api_url = 'https://api.vk.com/method/'
 db_name = 'db.db'
 bot_name = 'geo_enroll_vk_bot'
 api_ver = '5.80'
+uon_key = ''
+default_uon_admin_id = ''
+time_zone_from_msk = 0  # часовой пояс относительно москвы
+port = 8089
