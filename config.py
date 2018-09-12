8b2e7b3c049b6382f0a19642246904f666f69e6b
JMFedorov <jmfedorov@gmail.com>
2018-09-12 09:13:21 +0300

    rewrite config

--- config.py
+++ config.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-token = '76c156aee6d663381831389817defc594ec923edd09ae8fb2cc160624858d091fbb0f6628ba12177bef54'
 
-confirmation_token = '7e0b4572' # Group confirmation token
-secret_key = 'vErYsecreT228' # Group secret key
-group_id = 169862413  # VK group id
+token = '3aafaba38a8ac9b6d5b1d4f8efab66412fcf88e7f94c97789523a08b72c30849ffcdeaf06cf0721f6ed9d'
+
+confirmation_token = '9721ec02' # Group confirmation token
+secret_key = 'utuyfdt4rrttfiwhcqwerx2325ddgg' # Group secret key
+group_id = 60508505  # VK group id
 admin_id = 259056624
 admin_name = 'Юрий Федоров'
 vk_api_url = 'https://api.vk.com/method/'
@@ -15,4 +16,24 @@ api_ver = '5.80'
 uon_key = ''
 default_uon_admin_id = ''
 time_zone_from_msk = 0  # часовой пояс относительно москвы
-port = 8089
+port = ''
+
+#
+#
+#
+#
+# token = '76c156aee6d663381831389817defc594ec923edd09ae8fb2cc160624858d091fbb0f6628ba12177bef54'
+#
+# confirmation_token = '7e0b4572' # Group confirmation token
+# secret_key = 'vErYsecreT228' # Group secret key
+# group_id = 169862413  # VK group id
+# admin_id = 259056624
+# admin_name = 'Юрий Федоров'
+# vk_api_url = 'https://api.vk.com/method/'
+# db_name = 'db.db'
+# bot_name = 'geo_enroll_vk_bot'
+# api_ver = '5.80'
+# uon_key = ''
+# default_uon_admin_id = ''
+# time_zone_from_msk = 0  # часовой пояс относительно москвы
+# port = 8089
